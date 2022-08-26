a2da90cc65d656c371c58eea44566f307e4415d2
Rafael Pilartes <rafaelpilartes.rlps@gmail.com>
2022-08-26 05:02:19 +0100

    Dashboard -> controller upload img and create news

--- app/controllers/newsControllers.php
+++ app/controllers/newsControllers.php
@@ -2,24 +2,59 @@
 
 include_once('../db/config.php');
 
-if (isset($_POST['add_category'])) {
+if (isset($_POST['add_news'])) {
 
-  echo 'add_category';
+  echo 'add_news';
+
+  $size_max = 2097152; //2MB
+  $accept  = array("jpg", "png", "jpeg");
+  $extension  = pathinfo($_FILES['image_news']['name'], PATHINFO_EXTENSION);
+
+  // $dateNew = date('d-m-Y H:i:s');
+  // $dateNewComplete = "../_imageDB/date-$dateNew/";
+
+  if ($_FILES['image_news']['size'] >= $size_max) {
+    echo "Arquivo excedeu o tamanho máximo de 2MB";
+  } else {
+    if (in_array($extension, $accept)) {
+      // echo "Permitido";
+      $folder = "../_imagesDb/";
+
+      if (!is_dir($folder)) {
+        mkdir($folder, 755, true);
+      }
+
+      // Nome temporário do arquivo
+      $tmp = $_FILES['image_news']['tmp_name'];
+      // Novo nome do arquivo
+      $newName = "img_news-" . date('d-m-Y') . '-' . date('H') . 'h-' . uniqid() . ".$extension";
+
+      if (move_uploaded_file($tmp, $folder . $newName)) {
+        echo "Upload realizado com sucesso!";
+      } else {
+        echo "Erro: ao realizar Upload...";
+      }
+    } else {
+      echo "Erro: Extensão ($extension) não permitido";
+    }
+  }
+
 
   $title_news = $_POST['title_news'];
   $resume_news = $_POST['resume_news'];
   $author_id = $_POST['author_id'];
   $category_id = $_POST['category_id'];
-  $description_news = $_POST['descripton_news'];
+  $description_news = $_POST['description_news'];
   $epigraph_news = $_POST['epigraph_news'];
-  $image_news = $_POST['image_news'];
+  $author_epigraph_news = $_POST['author_epigraph_news'];
+  $image_news =  'http://localhost/web-pungo-andongo-php/app/_imagesDb/' . $newName;
   $description_image_news = $_POST['description_image_news'];
   $photography_news = $_POST['photography_news'];
   $reading_time_news = $_POST['reading_time_news'];
   $date_create = date('d-m-Y');
   $date_update =  date('d-m-Y');
 
-  $sql = $pdo->prepare("INSERT INTO news values(null,?,?,?,?,?,?,?,?,?,?,?,?)");
+  $sql = $pdo->prepare("INSERT INTO news values(null,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
   if ($sql->execute(array(
     $title_news,
@@ -28,6 +63,7 @@ if (isset($_POST['add_category'])) {
     $category_id,
     $description_news,
     $epigraph_news,
+    $author_epigraph_news,
     $image_news,
     $description_image_news,
     $photography_news,
--- app/views/dashboard/news.php
+++ app/views/dashboard/news.php
@@ -6,39 +6,40 @@
 
   <h1>Noticia</h1>
 
-  <form method="post" action="<?= urlProject(CONTROLLERS . "/newsControllers.php") ?>">
-    <div>
-      <input type="text" name="title_news" placeholder="Digite o titulo da Noticia" require>
-      <br>
+  <form method="post" enctype="multipart/form-data" action="<?= urlProject(CONTROLLERS . "/newsControllers.php") ?>">
+    <input type="text" name="title_news" placeholder="Digite o titulo da Noticia" require>
+    <br>
 
-      <input type="text" name="resume_news" placeholder="Digite o resumo da Noticia" require>
-      <br>
+    <input type="text" name="resume_news" placeholder="Digite o resumo da Noticia" require>
+    <br>
 
-      <input type="text" name="author_id" placeholder="Digite o id do autor" require>
-      <br>
+    <input type="number" name="author_id" placeholder="Digite o id do autor" require>
+    <br>
 
-      <input type="text" name="category_id" placeholder="Digite o id da categoria" require>
-      <br>
+    <input type="number" name="category_id" placeholder="Digite o id da categoria" require>
+    <br>
 
-      <input type="text" name="description_news" placeholder="Digite a descrição  da Noticia" require>
-      <br>
+    <input type="text" name="description_news" placeholder="Digite a descrição  da Noticia" require>
+    <br>
 
-      <input type="text" name="epigraph_news" placeholder="Digite epigrafe da Noticia" require>
-      <br>
+    <input type="text" name="epigraph_news" placeholder="Digite epigrafe da Noticia" require>
+    <br>
 
-      <input type="text" name="image_news" placeholder="Selecione a imagem da Noticia" require>
-      <br>
+    <input type="text" name="author_epigraph_news" placeholder="Digite nome do author da epigrafe" require>
+    <br>
 
-      <input type="text" name="description_image_news" placeholder="Digite a descrição a imagem" require>
-      <br>
+    <input type="file" name="image_news" placeholder="Selecione a imagem da Noticia" require>
+    <br>
 
-      <input type="text" name="photography_news" placeholder="Digite o nome do fotografo" require>
-      <br>
+    <input type="text" name="description_image_news" placeholder="Digite a descrição a imagem" require>
+    <br>
 
-      <input type="text" name="reading_time_news" placeholder="Digite o tempo de leitura" require>
-      <br>
+    <input type="text" name="photography_news" placeholder="Digite o nome do fotografo" require>
+    <br>
 
-    </div>
+    <input type="text" name="reading_time_news" placeholder="Digite o tempo de leitura" require>
+    <br>
+
 
     <div>
       <button name="add_news">
